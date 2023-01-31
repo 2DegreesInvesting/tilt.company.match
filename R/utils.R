300c25cf28140fb8a09ee0cd6eb714f325d18353
delacombazlinda <delacombaz.linda@gmail.com>
2023-01-31 15:38:11 +0000

    2dii dep

--- R/utils.R
+++ R/utils.R
@@ -94,11 +94,6 @@ to_alias <- function(x,
 
   out <- may_remove_ownership(remove_ownership, ownership, .init = out)
 
-  # remove inconsistent legal designation like "s.l.", "s l" and replace it wit "sl"
-  legal_designation_patterns = "(?<=\\s)s(\\.?l|l|\\sl|)\\.?"
-
-  out <- stringr::str_replace_all(out, legal_designation_patterns, "sl")
-
   # final adjustments
   out <- gsub("-", " ", out)
   out <- gsub("[[:space:]]", "", out)
