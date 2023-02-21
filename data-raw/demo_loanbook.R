261652f3d522c9df0e225c50e28f912420ef224c
Mauro Lepore <maurolepore@gmail.com>
2023-02-21 17:56:47 -0300

    52 Polish (#55)

---
+++ data-raw/demo_loanbook.R
@@ -0,0 +1,33 @@
+# styler: off
+demo_loanbook <- tibble::tribble(
+  ~id,             ~company_name, ~postcode,  ~country, ~misc_info,
+    # perfect name match, same postcode, same company to a tilt-demo entry
+    1,           "Peasant Peter",   "01234", "germany",        "A",
+    # same name and postcode but different company to other entry
+    2,           "Peasant Peter",   "01234", "germany",        "Z",
+    # same name  but different postcode and company to other entry
+    3,           "Peasant Peter",   "11234", "germany",        "Z",
+    # perfect name match, same postcode, different company to a tilt-demo entry
+    4,            "Peasant Paul",   "01234", "germany",        "Z",
+    # similar name, same postcode, same company to a tilt-demo entry
+    5,    "Bread Bakers Limited",   "23456", "germany",        "C",
+    # similar name, same postcode, different company to a tilt-demo entry
+    6,  "Flower Power & Company",   "34567", "germany",        "Z",
+    # different name, same postcode, different company to all tilt-demo entries
+    7,     "Screwdriver Experts",   "45678", "germany",        "D",
+    # similar name and same postcode but different company to other entry
+    8,      "Screwdriver Expert",   "45678", "germany",        "Z",
+    # different name, same postcode, same company to a tilt-demo entry (e.g. rename)
+    9,  "John Meier's Groceries",   "56789", "germany",        "E",
+    # same name but different postcode and different company to other entry,
+   10,  "John Meier's Groceries",   "55555", "germany",        "Y",
+    # not in tilt data
+   11,  "John Meier's Groceries",   "55555",  "norway",        "Y",
+    # only company in same zip, different name
+   12,             "Best Bakers",   "65656",  "france",        "F",
+   13,   "Concrete Incorporated",        NA,  "france",        "G",
+   14,      "Ur web consultants",        NA,        NA,        "H"
+)
+# styler: on
+
+usethis::use_data(demo_loanbook, overwrite = TRUE)
---
+++ tests/testthat/_snaps/demo_tilt.md
@@ -0,0 +1,22 @@
+# hasn't changed
+
+    Code
+      demo_tilt
+    Output
+      # A tibble: 13 x 5
+            id company_name                 postcode country misc_info
+         <dbl> <chr>                        <chr>    <chr>   <chr>    
+       1     1 Peasant Peter                01234    germany A        
+       2     2 Peasant Peter                01234    germany Z        
+       3     3 Peasant Peter                11234    germany Z        
+       4     4 Peasant Paul                 01234    germany B        
+       5     5 The Bread Bakers Ltd         23456    germany C        
+       6     6 Flower Power Friends and Co. 34567    germany D        
+       7     7 Flower Power and Co.         34567    germany F        
+       8     8 John and Jacques Groceries   56789    germany E        
+       9     9 John and Jacques Groceries   98765    germany E        
+      10    10 John and Jacques Groceries   98765    france  E        
+      11    11 Cranes and Friends           65656    france  F        
+      12    12 Concrete Inc                 12345    france  G        
+      13    13 Your web consultants         12345    france  H        
+
