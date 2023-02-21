388555cb25a2a703b54f9465199e262333a26efe
Mauro Lepore <maurolepore@gmail.com>
2023-02-21 20:27:17 +0000

    Style demo_tilt

--- data-raw/demo-tilt.R
+++ data-raw/demo-tilt.R
@@ -1,18 +1,26 @@
+# styler: off
 demo_tilt <- tibble::tribble(
-  ~id, ~company_name, ~postcode, ~country, ~misc_info,
-  1, "Peasant Peter", "01234", "germany", "A",
-  2, "Peasant Peter", "01234", "germany", "Z", # same name and postcode but different company to other entry
-  3, "Peasant Peter", "11234", "germany", "Z", # same name but different postcode and company to other entry
-  4, "Peasant Paul", "01234", "germany", "B",
-  5, "The Bread Bakers Ltd", "23456", "germany", "C",
-  6, "Flower Power Friends and Co.", "34567", "germany", "D",
-  7, "Flower Power and Co.", "34567", "germany", "F", # similar name and same postcode but different company to other entry
-  8, "John and Jacques Groceries", "56789", "germany", "E",
-  9, "John and Jacques Groceries", "98765", "germany", "E", # same name but different postcode and different company to other entry
-  10, "John and Jacques Groceries", "98765", "france", "E",
-  11, "Cranes and Friends", "65656", "france", "F",
-  12, "Concrete Inc", "12345", "france", "G", # company with high name match in loanbook
-  13, "Your web consultants", "12345" , "france", "H" # company with high name match in loanbook
+  ~id,                  ~company_name, ~postcode,  ~country, ~misc_info,
+    1,                "Peasant Peter",   "01234", "germany",        "A",
+    # same name and postcode but different company to other entry
+    2,                "Peasant Peter",   "01234", "germany",        "Z",
+    # same name but different postcode and company to other entry
+    3,                "Peasant Peter",   "11234", "germany",        "Z",
+    4,                 "Peasant Paul",   "01234", "germany",        "B",
+    5,         "The Bread Bakers Ltd",   "23456", "germany",        "C",
+    6, "Flower Power Friends and Co.",   "34567", "germany",        "D",
+    # similar name and same postcode but different company to other entry
+    7,         "Flower Power and Co.",   "34567", "germany",        "F",
+    8,   "John and Jacques Groceries",   "56789", "germany",        "E",
+    # same name but different postcode and different company to other entry
+    9,   "John and Jacques Groceries",   "98765", "germany",        "E",
+   10,   "John and Jacques Groceries",   "98765",  "france",        "E",
+   11,           "Cranes and Friends",   "65656",  "france",        "F",
+    # company with high name match in loanbook
+   12,                 "Concrete Inc",   "12345",  "france",        "G",
+    # company with high name match in loanbook
+   13,         "Your web consultants",   "12345",  "france",        "H"
 )
+# styler: on
 
 usethis::use_data(demo_tilt, overwrite = TRUE)
