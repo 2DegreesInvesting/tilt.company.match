5471b86372620306774b767556661f2bdcbc1927
Mauro Lepore <maurolepore@gmail.com>
2023-02-22 14:37:04 +0000

    Add expectation to show this really makes a difference

--- tests/testthat/test-duplicated_paste.R
+++ tests/testthat/test-duplicated_paste.R
@@ -1,4 +1,8 @@
 testthat::test_that("returns correct booleans", {
   data <- tibble(x = c("company_1", "company_1"), y = c("01", "02"))
   expect_equal(duplicated_paste(data$x, data$y), c(FALSE, FALSE))
+  expect_false(identical(
+    duplicated(data$x, data$y),
+    duplicated_paste(data$x, data$y)
+  ))
 })
