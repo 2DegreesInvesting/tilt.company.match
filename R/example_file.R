b9f4a7d16030c3f289bae391e0dd3a941671a340
Mauro Lepore <maurolepore@gmail.com>
2023-02-23 21:22:16 +0000

    Export

--- R/example_file.R
+++ R/example_file.R
@@ -1,3 +1,16 @@
+#' Get the path to an example file
+#'
+#' @param file Name of the file.
+#'
+#' @return A path.
+#' @export
+#'
+#' @examples
+#' example_file("demo_loanbook.csv")
+#'
+#' example_file("demo_tilt.csv")
+#'
+#' example_file("demo_matched.csv")
 example_file <- function(file) {
   system.file("extdata", file, package = "tilt.company.match", mustWork = TRUE)
 }
