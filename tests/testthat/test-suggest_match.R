8e93bc6ca42af10965d51c830ab8e4b3b3288be8
Mauro Lepore <maurolepore@gmail.com>
2023-03-01 22:00:42 +0000

    refactor

--- tests/testthat/test-suggest_match.R
+++ tests/testthat/test-suggest_match.R
@@ -3,6 +3,5 @@ test_that("writes the expected file", {
     loanbook_csv = example_file("demo_loanbook.csv"),
     tilt_csv = example_file("demo_tilt.csv")
   )
-  ref <- read(test_path("data", "to_edit.csv"))
-  expect_equal(out, ref)
+  expect_equal(out, read(test_path("data", "to_edit.csv")))
 })
