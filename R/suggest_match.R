a705f179f4536f5c7eac97a7dad881655e6fccda
Mauro Lepore <maurolepore@gmail.com>
2023-03-01 21:36:23 +0000

    Refactor

--- R/suggest_match.R
+++ R/suggest_match.R
@@ -1,11 +1,11 @@
-suggest_match <- function() {
+suggest_match <- function(.params = NULL) {
   rmd <- fs::path_package("templates", "1.Rmd", package = "tilt.company.match")
   dir <- withr::local_tempdir()
   withr::local_dir(dir)
 
   tmp_rmd <- fs::path(dir, fs::path_file(rmd))
   fs::file_copy(rmd, tmp_rmd)
-  rmarkdown::render(tmp_rmd, quiet = TRUE)
+  rmarkdown::render(tmp_rmd, params = .params, quiet = TRUE)
 
   out <- read("to_edit.csv")
   out
