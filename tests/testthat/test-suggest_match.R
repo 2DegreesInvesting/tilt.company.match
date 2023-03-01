4bf1199c74e9438bce56f5f646a26bd271c62e7b
Mauro Lepore <maurolepore@gmail.com>
2023-03-01 21:38:43 +0000

    Refactor

--- tests/testthat/test-suggest_match.R
+++ tests/testthat/test-suggest_match.R
@@ -1,4 +1,5 @@
 test_that("writes the expected file", {
-  out <- suggest_match()
+  .params <- NULL
+  out <- suggest_match(.params)
   expect_equal(out, read(test_path("data", "to_edit.csv")))
 })
