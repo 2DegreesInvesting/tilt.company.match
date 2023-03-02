58c874243bcf3b086761364790df23367bca9a90
Mauro Lepore <maurolepore@gmail.com>
2023-03-02 20:10:04 +0000

    Refactor

--- R/suggest_match.R
+++ R/suggest_match.R
@@ -1,16 +1,119 @@
-suggest_match <- function(loanbook_csv, tilt_csv) {
-  rmd <- fs::path_package("templates", "1.Rmd", package = "tilt.company.match")
-  dir <- withr::local_tempdir()
-  withr::local_dir(dir)
+#' Given a loanbook and tilt datasets returns a dataset with suggested matches
+#'
+#' @param loanbook Path to a .csv file with your `loanbook` data.
+#' @param tilt Path to a .csv file with 2DII's `tilt` data.
+#' @param eligibility_threshold Eligibility threshold.
+#' @param suggestion_threshold Suggestion threshold.
+#'
+#' @return A dataframe with suggested matching candidates.
+#' @export
+#'
+#' @examples
+#' loanbook <- example_file("demo_loanbook.csv")
+#' loanbook
+#'
+#' tilt <- example_file("demo_tilt.csv")
+#' tilt
+#'
+#' suggest_match(loanbook, tilt)
+suggest_match <- function(loanbook,
+                          tilt,
+                          eligibility_threshold = 0.75,
+                          suggestion_threshold = 0.9) {
+  loanbook <- vroom(loanbook, show_col_types = FALSE)
+  tilt <- vroom(tilt, show_col_types = FALSE)
 
-  tmp_rmd <- fs::path(dir, fs::path_file(rmd))
-  fs::file_copy(rmd, tmp_rmd)
+  # TODO: Extract checks
+  expected <- c("id", "company_name", "postcode", "country")
+  loanbook %>% check_crucial_names(expected)
 
-  .params <- list(loanbook_csv = loanbook_csv, tilt_csv = tilt_csv)
-  rmarkdown::render(tmp_rmd, params = .params, quiet = TRUE)
+  has_no_duplicates <- identical(anyDuplicated(loanbook$id), 0L)
+  stopifnot(has_no_duplicates)
 
-  out <- read("to_edit.csv")
-  out
+  best_without_duplicates <- c("company_name", "postcode", "country")
+  report_duplicates(loanbook, best_without_duplicates)
+
+  non_nullable <- c("id", "company_name")
+  loanbook %>% abort_if_incomplete(non_nullable)
+
+  loanbook_alias <- loanbook %>% mutate(company_alias = to_alias(.data$company_name))
+  # TODO: We can pre-compute this before we send the tilt dataset
+  tilt_alias <- tilt %>% mutate(company_alias = to_alias(.data$company_name))
+
+  # TODO: Ignore grouping if reading and matching line by line
+  lacks_none <- loanbook_alias %>%
+    filter(!is.na(.data$postcode) & !is.na(.data$country)) %>%
+    left_join(
+      tilt_alias,
+      by = c("country", "postcode"),
+      suffix = c("", "_tilt"),
+      multiple = "all"
+    )
+
+  lacks_postcode <- loanbook_alias %>%
+    filter(is.na(.data$postcode) & !is.na(.data$country)) %>%
+    left_join(
+      tilt_alias,
+      by = c("country"),
+      suffix = c("", "_tilt"),
+      multiple = "all"
+    )
+
+  lacks_country <- loanbook_alias %>%
+    filter(!is.na(.data$postcode) & is.na(.data$country)) %>%
+    left_join(tilt_alias, by = c("postcode"), suffix = c("", "_tilt"))
+
+  lacks_both <- loanbook_alias %>%
+    filter(is.na(.data$postcode) & is.na(.data$country)) %>%
+    mutate(postcode = "join_helper") %>%
+    inner_join(
+      dplyr::mutate(tilt_alias, postcode = "join_helper"),
+      by = c("postcode"),
+      suffix = c("", "_tilt"),
+      multiple = "all"
+    ) %>%
+    mutate(postcode = NA_character_)
+
+  candidates <- bind_rows(lacks_none, lacks_postcode, lacks_country, lacks_both)
+
+  okay_candidates <- candidates %>%
+    # Other parameters may perform best. See `?stringdist::stringsim`
+    mutate(similarity = stringsim(
+      .data$company_alias, .data$company_alias_tilt,
+      # Good to compare human typed text that might have typos.
+      method = "jw",
+      p = 0.1
+    )) %>%
+    # Arrange matching candidates from more to less similar
+    arrange(id, -.data$similarity)
+
+  best_candidates <- okay_candidates %>%
+    filter(.data$similarity > eligibility_threshold | is.na(.data$similarity))
+
+  unmatched <- anti_join(
+    okay_candidates %>% distinct(id, .data$company_name),
+    best_candidates %>% distinct(id, .data$company_name)
+  )
+
+  candidates_suggest_match <- best_candidates %>%
+    # - It's the highest among all other candidates.
+    group_by(id) %>%
+    filter(.data$similarity == max(.data$similarity)) %>%
+    # - It's above the threshold.
+    filter(.data$similarity > suggestion_threshold) %>%
+    # - It's the only such highest value in the group defined by a combination of
+    # `company_name` x `postcode` -- to avoid duplicates.
+    mutate(duplicates = any(duplicated_paste(.data$company_name, .data$postcode))) %>%
+    filter(!.data$duplicates) %>%
+    select("id", "id_tilt") %>%
+    mutate(suggest_match = TRUE) %>%
+    ungroup()
+
+  to_edit <- best_candidates %>%
+    left_join(candidates_suggest_match, by = c("id", "id_tilt")) %>%
+    mutate(accept_match = NA)
+
+  to_edit
 }
 
 read <- function(...) {
--- tests/testthat/test-suggest_match.R
+++ tests/testthat/test-suggest_match.R
@@ -1,7 +1,10 @@
-test_that("writes the expected file", {
+test_that("hasn't changed", {
   out <- suggest_match(
-    loanbook_csv = example_file("demo_loanbook.csv"),
-    tilt_csv = example_file("demo_tilt.csv")
-  )
-  expect_equal(out, read(test_path("data", "to_edit.csv")))
+    loanbook = example_file("demo_loanbook.csv"),
+    tilt = example_file("demo_tilt.csv")
+  ) |>
+    # FIXME: Implement `quietly`
+    suppressMessages()
+
+  expect_snapshot(as.data.frame(out))
 })
