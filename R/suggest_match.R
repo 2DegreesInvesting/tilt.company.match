72f4ed68a4e7f02607b718326853ef1ce2673afc
Mauro Lepore <maurolepore@gmail.com>
2023-03-06 23:20:02 +0000

    Move data checks to check_loanbook()

--- R/suggest_match.R
+++ R/suggest_match.R
@@ -151,9 +151,26 @@ suggest_match <- function(loanbook,
 #'
 #' @examples
 #' library(vroom)
+#' library(dplyr, warn.conflicts = FALSE)
 #'
 #' loanbook <- vroom(example_file("demo_loanbook.csv"), show_col_types = FALSE)
 #' check_loanbook(loanbook)
+#'
+#' # Do you have the expected columns?
+#' bad_name <- rename(loanbook, ids = id)
+#' try(check_loanbook(bad_name))
+#'
+#' # Do you have any duplicates in the column `id`?
+#' bad_id <- bind_rows(loanbook, slice(loanbook, 1))
+#' try(check_loanbook(bad_id))
+#'
+#' # Do you have missing values (`NA`s) in non-nullable columns?
+#' missing_id <- tribble(
+#'   ~id,            ~company_name, ~postcode,  ~country, ~misc_info,
+#'    NA, "John Meier's Groceries",   "55555", "germany",        "Y",
+#'    11, "John Meier's Groceries",   "55555",  "norway",        "Y"
+#' )
+#' try(check_loanbook(missing_id))
 check_loanbook <- function(loanbook) {
   expected <- c("id", "company_name", "postcode", "country")
   loanbook %>% check_crucial_names(expected)
