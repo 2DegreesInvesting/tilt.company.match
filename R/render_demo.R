7f699708c93b94285048d2ae7ed1e4c3bc1318f2
Mauro Lepore <maurolepore@gmail.com>
2023-03-09 23:40:52 +0000

    Document

--- R/render_demo.R
+++ R/render_demo.R
@@ -1,4 +1,8 @@
 #' Render a .Rmd file into a .md file under tests/testthat/demos
+#'
+#' One use case of this  function is when you are working on a PR and want to
+#' share the output of an .Rmd file. Once done would delete the .md file and
+#' merge the PR.
 #' @noRd
 #' @examples
 #' render_demo("vignettes/articles/tilt-company-match.Rmd")
