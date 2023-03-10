4d5f8daf6bd0f4c09a253913850ef553bb8d0f04
Mauro Lepore <maurolepore@gmail.com>
2023-03-10 23:10:21 +0000

    Document

--- R/pick_unmatched.R
+++ R/pick_unmatched.R
@@ -1,3 +1,26 @@
+#' Pick unmatched `id` and `company_name` from a loanbook
+#'
+#' @param loanbook A data frame with columns `id` and `company_name`.
+#' @param accepted A data frame with columns `id` and `accept_match` (of type
+#'   logical).
+#'
+#' @return A data frame with columns `id` and `company_name` reflecting the
+#'   `looanbook` rows that don't match the `accepted` data frame.
+#' @export
+#'
+#' @examples
+#' library(tibble)
+#'
+#' loanbook <- tibble(id = 1:2, company_name = letters[id], irrelevant = "xyz")
+#' loanbook
+#'
+#' accepted <- tibble(id = 1:2, accept_match = c(TRUE, FALSE))
+#' accepted
+#'
+#' pick_unmatched(loanbook, accepted)
+#'
+#' bad_type <- tibble(id = 1, accept_match = "TRUE")
+#' pick_unmatched(loanbook, bad_type)
 pick_unmatched <- function(loanbook, accepted) {
   check_crucial_names(loanbook, c("id", "company_name"))
   check_crucial_names(accepted, c("id", "accept_match"))
