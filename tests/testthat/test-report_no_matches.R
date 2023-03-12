8191af1f491cfa97b6e6f597d352638eafaec2da
Mauro Lepore <maurolepore@gmail.com>
2023-03-12 16:01:22 +0000

    refactor

--- tests/testthat/test-report_no_matches.R
+++ tests/testthat/test-report_no_matches.R
@@ -25,12 +25,12 @@ test_that("without crucial columns errors gracefully", {
 
 test_that("without crucial datasets errors gracefully", {
   loanbook <- read_example("demo_loanbook.csv")
-  accepted <- read_example("demo_matched.csv")
+  matched <- read_example("demo_matched.csv")
 
-  expect_no_error(report_no_matches(loanbook, accepted))
+  expect_no_error(report_no_matches(loanbook, matched))
   expect_error(report_no_matches(loanbook), "matched.*missing.*no default")
   expect_error(
-    report_no_matches(manually_matched = accepted),
+    report_no_matches(manually_matched = matched),
     "loanbook.*missing.*no default"
   )
 })
@@ -53,13 +53,13 @@ test_that("with 1 unmatched company returns 1 row with that company", {
   loanbook <- tibble(id = 1:2, company_name = c("a", "b"))
 
   .accept_match <- c(TRUE, NA)
-  accepted <- tibble(id = 1:2, accept_match = .accept_match)
-  out <- report_no_matches(loanbook, accepted)
+  matched <- tibble(id = 1:2, accept_match = .accept_match)
+  out <- report_no_matches(loanbook, matched)
   expect_equal(out, tibble(id = 2, company_name = "b"))
 
   # Same
   .accept_match <- c(TRUE, FALSE)
-  accepted <- tibble(id = 1:2, accept_match = .accept_match)
-  out <- report_no_matches(loanbook, accepted)
+  matched <- tibble(id = 1:2, accept_match = .accept_match)
+  out <- report_no_matches(loanbook, matched)
   expect_equal(out, tibble(id = 2, company_name = "b"))
 })
