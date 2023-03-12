e7b23ca11458f7d94593ed3c27dfee1fb2ffc36a
Mauro Lepore <maurolepore@gmail.com>
2023-03-12 23:08:31 +0000

    flag fixme

--- tests/testthat/test-suggest_match.R
+++ tests/testthat/test-suggest_match.R
@@ -18,6 +18,14 @@ test_that("with no match outputs 0-rows", {
   expect_equal(nrow(out), 0L)
 })
 
+test_that("with no match throws no warning", {
+  skip("FIXME #131")
+  expect_no_warning(
+    suggest_match(toy(), toy(id = 2, company_name = "x")),
+    "no non-missing arguments to max; returning -Inf"
+  )
+})
+
 test_that("additional columns appear in the output", {
   expect_true(hasName(suggest_match(toy(x = 1), toy(y = 1)), "x"))
   expect_true(hasName(suggest_match(toy(x = 1), toy(y = 1)), "y"))
@@ -32,8 +40,26 @@ test_that("with 1 match in a 2-row loanbook outputs the 1 matching company", {
   expect_equal(out$company_name, "b")
 })
 
-test_that("", {
- skip("TODO characterize thresholds")
+test_that("is sensitive to `suggestion_treshold", {
+  skip("FIXME #132")
+  l <- toy(id = 1:3, company_name = c("aaaaa", "aaaab", "aaabb"))
+  t <- toy(id = 9, company_name = c("aaaaa"))
+
+  # The threshold should be inclusive.
+  .suggestion_threshold <- 1
+  out <- suggest_match(l, t, suggestion_threshold = .suggestion_threshold)
+  filter(out, similarity >= .suggestion_threshold) |>
+    select(similarity, company_name, suggest_match) |>
+    pull(suggest_match) |>
+    expect_true()
+
+  # This works but is not intuitive
+  .suggestion_threshold <- .99
+  out <- suggest_match(l, t, suggestion_threshold = .suggestion_threshold)
+  filter(out, similarity >= .suggestion_threshold) |>
+    select(similarity, company_name, suggest_match) |>
+    pull(suggest_match) |>
+    expect_true()
 })
 
 test_that("", {
